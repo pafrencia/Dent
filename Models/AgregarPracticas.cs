70e5e753beddf10ac42c097a2d1b1c58b1e41f9d
pafrencia@gmail.com <pafrencia@gmail.com>
2024-02-23 20:43:01 -0300

    sigue andando y tiene la base de datos conectada y creados los metodos de entity

---
+++ Models/AgregarPracticas.cs
@@ -0,0 +1,13 @@
+﻿namespace Dent.Models
+{
+    public class AgregarPracticas
+    {
+
+        public void CargarPractica()
+        {
+            var pedro = new Practica();
+            pedro.Practica1 = "nueva Pracica";
+            pedro.Precio = 123;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Dent.Data;
+using Dent.Models;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -18,7 +19,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
