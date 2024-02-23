b3ec5fc61e5327b00d004e9110e3e43880bf9f70
pafrencia@gmail.com <pafrencia@gmail.com>
2024-02-23 15:00:43 -0300

    Add project files.

---
+++ Models/DbDentistaContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dent.Models;
+
+public partial class DbDentistaContext : DbContext
+{
+    public DbDentistaContext()
+    {
+    }
+
+    public DbDentistaContext(DbContextOptions<DbDentistaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Practica> Practicas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-S5MA4R8; Database=DbDentista; Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Practica>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            entity.Property(e => e.Practica1)
+                .HasMaxLength(30)
+                .IsFixedLength()
+                .HasColumnName("Practica");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Practica.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dent.Models;
+
+public partial class Practica
+{
+    public int Id { get; set; }
+
+    public string? Practica1 { get; set; }
+
+    public int? Precio { get; set; }
+}
