75e0d371be849c6d89a9f479e2610a640e3dfd9c
pafrencia@gmail.com <pafrencia@gmail.com>
2024-02-27 15:27:50 -0300

    cargado todo con las funciones de pracios

---
+++ Models/Resultados.cs
@@ -0,0 +1,10 @@
+﻿namespace Dent.Models
+{
+    public class Resultados
+    {
+        public int Valor1 { get; set; }
+        public int Valor2 { get; set; }
+        public int Valor3 { get; set; }
+        public int Valor4 { get; set; }
+    }
+}
