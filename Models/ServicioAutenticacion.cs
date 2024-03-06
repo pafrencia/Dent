835ee38551274abdde0b49197e784628473d9527
pafrencia@gmail.com <pafrencia@gmail.com>
2024-03-06 01:39:23 -0300

    Inicia Sesion ok

--- Models/ServicioAutenticacion.cs
+++ Models/ServicioAutenticacion.cs
@@ -1,13 +1,50 @@
 ﻿using Dent.Pages;
+using Microsoft.AspNetCore.Components;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace Dent.Models
 {
+
+
     public class ServicioAutenticacion
     {
         // Método para validar las credenciales del usuario
+
+        [Inject]
+        private DbDentistaContext DbContext { get; set; }
+
+
+
         public bool ValidarCredenciales(string nombreUsuario, string contraseña)
         {
 
-            return nombreUsuario == "Pablo" && contraseña == "123";
+            var entidad = new Login();
+ 
+            entidad = DbContext.Logins.SingleOrDefault(e => e.Usuario == nombreUsuario);
+
+
+                    if (entidad != null)
+                {
+                    // Obtener la clave primaria
+             //       var clavePrimaria = DbContext.Entry(entidad).Property("Id").CurrentValue;
+                    return true;
+                    
+                }
+                else
+                
+                    return false;
+                }
+                    
+
+                
+            }
+
+
         }
-    }
-}
+
+
+
+
+    
+
