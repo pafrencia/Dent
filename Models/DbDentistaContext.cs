8f80f08a5558ba352e6f0c2286a4a2717364d44c
pafrencia@gmail.com <pafrencia@gmail.com>
2024-03-07 01:14:39 -0300

    Ya hay dos tablas con obras sociales diferentes y funciona todo OK

--- Models/DbDentistaContext.cs
+++ Models/DbDentistaContext.cs
@@ -19,6 +19,8 @@ public partial class DbDentistaContext : DbContext
 
     public virtual DbSet<Practica1> Practicas { get; set; }
 
+    public virtual DbSet<PracticasApro> PracticasApros { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=DESKTOP-S5MA4R8; Database=DbDentista; Trusted_Connection=True;TrustServerCertificate=True;");
@@ -43,6 +45,13 @@ public partial class DbDentistaContext : DbContext
                 .HasMaxLength(30)
                 .IsFixedLength()
                 .HasColumnName("Practica");
+        });
+
+        modelBuilder.Entity<PracticasApro>(entity =>
+        {
+            entity.Property(e => e.Practica)
+                .HasMaxLength(30)
+                .IsFixedLength();
         });
 
         OnModelCreatingPartial(modelBuilder);
---
+++ Models/PracticasApro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dent.Models;
+
+public partial class PracticasApro
+{
+    public int Id { get; set; }
+
+    public string Practica { get; set; }
+
+    public int Precio { get; set; }
+}
